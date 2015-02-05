9e7b10a6b60a73fbdd2722d27acdc407c60500cf
Stephen Roebuck <sroebuck@veracode.com>
2015-02-05 15:29:49 -0500

    first bracnh commit

--- main/src/main/stephen/StephensHash.java
+++ main/src/main/stephen/StephensHash.java
@@ -2,8 +2,13 @@ package main.stephen;
 
 import main.interfaces.Hash;
 
+import java.util.Set;
+
 /**
  * Created by Controllerface on 2/5/2015.
  */
 public class StephensHash implements Hash {
+    Set<String> jaccardSet(){
+        return null;
+    }
 }
