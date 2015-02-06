cc23f30f6094029e568774f806848d48892846a2
Stephen Roebuck <sroebuck@veracode.com>
2015-02-06 11:06:51 -0500

    trying to add gradle

--- main/src/main/data/WebPage.java
+++ main/src/main/data/WebPage.java
@@ -1,7 +1,16 @@
 package main.data;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Created by Controllerface on 2/5/2015.
  */
 public class WebPage {
+    public BufferedImage domImage(){
+        return null;
+    }
+
+    public String domString(){
+        return null;
+    }
 }
--- main/src/main/stephen/StephensHash.java
+++ main/src/main/stephen/StephensHash.java
@@ -8,7 +8,13 @@ import java.util.Set;
  * Created by Controllerface on 2/5/2015.
  */
 public class StephensHash implements Hash {
-    Set<String> jaccardSet(){
-        return null;
+    final Set<String> shingleSet;
+
+    public StephensHash(Set<String> shingleSet) {
+        this.shingleSet = shingleSet;
+    }
+
+    Set<String> shingleSet(){
+        return shingleSet;
     }
 }
